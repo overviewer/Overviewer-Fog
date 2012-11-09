9c4e3a5d808192956701523239a6b6031ea29e1d
Andrew Chin <achin@eminence32.net>
2012-11-09 18:53:01 -0500

    Convert gen_world to use new Job class.  Also cleanups

--- gen_world_from_seed.py
+++ gen_world_from_seed.py
@@ -2,39 +2,40 @@
 
 import sys
 import os
-import json
 import tempfile
 import subprocess
 import shutil
 
+from jobs import *
+
+
+class WorldGenJob(Job):
+    job_type = "worldgen"
+    job_fields = [
+        job_field("seed", '', str),
+        job_field("spawn", [], list)
+    ]
+
 try:
-    import redstone # <3 libredstone
+    import redstone  # <3 libredstone
 except ImportError:
     sys.path.append(os.path.expanduser("~/devel/libredstone/bindings"))
     import redstone
 
-try:
-    import boto
-except ImportError:
-    sys.path.append(os.path.expanduser("~/devel/boto"))
-    import boto
 
-from boto.sqs.connection import SQSConnection 
-from boto.sdb.connection import SDBConnection 
 from boto.s3.connection import S3Connection
-from boto.sqs.message import Message
 from boto.s3.key import Key
 
 # provide your own config.py that holds useful
 # configuration settings
 import config
 
-import uuid
 
 if "AWS_ACCESS_KEY_ID" not in os.environ:
     print "Please set the AWS_ACCESS_KEY_ID environment variable"
 if "AWS_SECRET_ACCESS_KEY" not in os.environ:
     print "Please set the AWS_SECRET_ACCESS_KEY environment variable"
+
 
 def submit():
     if len(sys.argv) < 3:
@@ -50,93 +51,51 @@ def submit():
         assert(len(spawn) == 3)
         print "Generate world with this seed (\"%s\") with spawn %r [y/N]?" % (seed, spawn)
     else:
-        spawn = None
+        spawn = []
         print "Generate world with this seed (\"%s\") [y/N]?" % seed
     if raw_input().lower() == 'y':
-        uid = uuid.uuid4()
-
-        print "Submitting job %s to queue..." % uid
-        sqs = SQSConnection()
-        sdb = SDBConnection()
-        queue = sqs.get_queue("overviewer-genfromseed")
-        db = sdb.get_domain("overviewerdb")
-        print queue
-        print db
-
-        data = dict()
-        data['uuid'] = str(uid)
-        data['seed'] = seed
-        data['generated'] = False
-        if spawn:
-            data['target_spawn'] = spawn
-        if not db.put_attributes(uid, data):
-            print "***Error: Failed to update the db"
-            return 1
-        
-        msg = Message()
-        msg.set_body(str(uid))
-        if not queue.write(msg):
-            print "***Error: Failed to enqueue"
-            return 1
+        job = WorldGenJob.submit(seed=seed, spawn=spawn)
 
         print "Ok, job enqueued"
-
+        print "job UUID:", job.uuid
 
     else:
         print "Ok, not submitting.  Bye"
         return
-    
+
 
 def generate():
-    pass
-    from boto.sqs.connection import SQSConnection 
-    sqs = SQSConnection()
 
-    queue = sqs.get_queue("overviewer-genfromseed")
-    sdb = SDBConnection()
-    db = sdb.get_domain("overviewerdb")
-
-    message = queue.read(visibility_timeout=15)
-    if not message:
+    job = WorldGenJob.fetch_next(15)
+    if job is None:
         print "Nothing in the queue.  Please try again later"
         return 0
-    uid = message.get_body()
-    print "Got a job for %r" % uid
-    data = db.get_item(uid)
 
-    if 'target_spawn' in data:
-        data['target_spawn'] = map(int, data['target_spawn'])
-    print data
+    seed = job.seed
+    spawn = job.spawn
 
-    # this script generate maps from seeds
-    # if this map is already generated, then upate the db
-    if data['generated'] == 'True':
-        print "---Warning: I was asked to generate this map, but it's already generated"
-        print "            I'm going to update the db, but not re-generate"
-        data['generated'] = True
-        data.save()
-        queue.delete_message(message)
-        return 
-
+    print "got job"
+    print "seed", seed
+    print "spawn", spawn
 
     # check config options
     if not os.path.isfile(config.minecraft_server):
         raise Exception("minecraft_server isn't configured")
 
     # with a generous amount of rounding up, assume it'll take 5 minutes to generate the map
-    message.change_visibility(5*60)
+    job.update(5 * 60)
 
     tmpdir = tempfile.mkdtemp(prefix="mc_gen")
 
     # create a settings.properties file with our seed
     with open(os.path.join(tmpdir, "server.properties"), "w") as f:
-        f.write("level-seed=%s" % data['seed'])
+        f.write("level-seed=%s" % seed)
 
     p = subprocess.Popen(["java", "-jar",
-        config.minecraft_server, "-noGUI"],
-        shell=False,
-        stdin=subprocess.PIPE,
-        cwd=tmpdir)
+                         config.minecraft_server, "-noGUI"],
+                         shell=False,
+                         stdin=subprocess.PIPE,
+                         cwd=tmpdir)
 
     p.stdin.write("stop\n")
     p.stdin.close()
@@ -147,31 +106,30 @@ def generate():
 
     # if we want a specific spawn, we need to rewrite the level.dat file,
     # remove the old region files, and restart the server
-    if 'target_spawn' in data:
-        s = data['target_spawn']
+    if len(spawn) > 0:
         leveldat = redstone.NBT.parse_from_file(os.path.join(tmpdir, "world", "level.dat"))
         root = leveldat.root
-        root['Data']['SpawnX'].set_integer(int(s[0]))
-        root['Data']['SpawnY'].set_integer(int(s[1]))
-        root['Data']['SpawnZ'].set_integer(int(s[2]))
+        root['Data']['SpawnX'].set_integer(int(spawn[0]))
+        root['Data']['SpawnY'].set_integer(int(spawn[1]))
+        root['Data']['SpawnZ'].set_integer(int(spawn[2]))
         leveldat.write_to_file(os.path.join(tmpdir, "world", "level.dat"))
 
-        shutil.rmtree(os.path.join(tmpdir,"world","region"))
+        shutil.rmtree(os.path.join(tmpdir, "world", "region"))
         p = subprocess.Popen(["java", "-jar",
-            config.minecraft_server, "-noGUI"],
-            shell=False,
-            stdin=subprocess.PIPE,
-            cwd=tmpdir)
+                             config.minecraft_server, "nogui"],
+                             shell=False,
+                             stdin=subprocess.PIPE,
+                             cwd=tmpdir)
 
         p.stdin.write("stop\n")
         p.stdin.close()
         p.wait()
         print "Minecraft server exited with %r" % p.returncode
 
-    message.change_visibility(5*60)
+    job.update(5 * 60)
     print "Making tarball..."
     p = subprocess.Popen(["tar", "-cf", "world.tar", "world/"],
-        cwd=tmpdir)
+                         cwd=tmpdir)
     p.wait()
     if p.returncode != 0:
         print "***Error: tar failed"
@@ -179,8 +137,8 @@ def generate():
     print "OK."
     print "Compressing..."
     p = subprocess.Popen(["bzip2", "world.tar"],
-            shell=False,
-            cwd=tmpdir)
+                         shell=False,
+                         cwd=tmpdir)
     p.wait()
     if p.returncode != 0:
         print "***Error: compress failed"
@@ -190,7 +148,7 @@ def generate():
     s3 = S3Connection()
     bucket = s3.get_bucket("overviewer-worlds")
     k = Key(bucket)
-    k.key = "%s.tar.bz2" % uid
+    k.key = "%s.tar.bz2" % job.uuid
     print "Uploading to S3..."
     k.set_contents_from_filename(os.path.join(tmpdir, "world.tar.bz2"), reduced_redundancy=True)
     print "OK."
@@ -200,17 +158,9 @@ def generate():
     url = urlbase + k.key
     print "World is now available at:", url
 
-    data['generated'] = True
-    data['world_url'] = url
-    data.save()
-    print "Database updated."
-
-    queue.delete_message(message)
+    job.finish()
 
     print "All done!"
-
-
-
 
 if __name__ == "__main__":
     if "-seed" in sys.argv:
@@ -221,4 +171,3 @@ if __name__ == "__main__":
         print "Usage:"
         print "  %s -seed <seed> [<spawn coords>]" % sys.argv[0]
         print "  %s -generate" % sys.argv[0]
-
